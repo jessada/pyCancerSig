b6425caee829bf753016924f675be0291711e357
Jessada <jessada.thutkawkorapin@ki.se>
2018-12-10 11:14:54 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pyCancerSig',
+    version='0.0.1',
+    author='Jessada Thutkawkorapin',
+    author_email='jessada.thutkawkorapin@gmail.com',
+    packages=['cancersig',
+              ],
+    package=['pyCancerSig'],
+    url='http://pypi.python.org/pypi/pyCancerSig/',
+    license='LICENSE.txt',
+    description='Python packages for deciphering cancer signature processes',
+    long_description=open('README.md').read(),
+)
